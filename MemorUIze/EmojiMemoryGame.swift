93ecb92c40c45dc1a3fb00acbf068ab486781914
diegosoriarios <diegosoriarios@gmail.com>
2020-06-29 14:00:36 -0300

    first commit

---
+++ MemorUIze/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  MemorUIze
+//
+//  Created by diego.rios on 29/06/20.
+//  Copyright © 2020 diego.rios. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    //MARK: - Intent
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ MemorUIze/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  MemorUIze
+//
+//  Created by diego.rios on 29/06/20.
+//  Copyright © 2020 diego.rios. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex*2))
+            cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+        var id: Int
+    }
+}
